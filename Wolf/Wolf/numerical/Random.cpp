1d7603527e00b54eeb4399756ac68e5ff8907589
FrancoYudica <francoyudica@gmail.com>
2023-07-09 10:44:42 -0300

    Random int generator

--- Wolf/Wolf/numerical/Random.cpp
+++ Wolf/Wolf/numerical/Random.cpp
@@ -47,63 +47,29 @@ double RandomDouble::range(double min, double max)
     return min + _distribution(_generator) * (max - min);
 }
 
-/*
-float Random::unit_f()
-{
-    static thread_local std::uniform_real_distribution<float> distrubution(0.0, 1.0);
-    static thread_local std::mt19937 generator;
-    return distrubution(generator);
-}
-float Random::range(float min, float max)
-{
-    return min + unit_f() * (max - min);
-}
-
-float Random::signed_unit_f()
-{
-    return unit_f() * 2.0f - 1.0f;
-}
 
 
-// FLOAT IMPLEMENTATIONS
-float Random::unit_f()
-{
-    static thread_local std::uniform_real_distribution<float> distrubution(0.0, 1.0);
-    static thread_local std::mt19937 generator;
-    return distrubution(generator);
-}
-float Random::range(float min, float max)
-{
-    return min + unit_f() * (max - min);
-}
-
-float Random::signed_unit_f()
-{
-    return unit_f() * 2.0f - 1.0f;
-}
-
-// DOUBLE IMPLEMENTATIONS
-double Random::unit_d()
-{
-    static thread_local std::uniform_real_distribution<double> distrubution(0.0, 1.0);
-    static thread_local std::mt19937 generator;
-    return distrubution(generator);
-}
-double Random::range(double min, double max)
-{
-    return min + unit_d() * (max - min);
-}
-
-double Random::signed_unit_d()
-{
-    return unit_d() * 2.0f - 1.0f;
-}
-
-int32_t Random::range(int32_t min, int32_t max)
-{
-    float random_float = Random::unit_f();
-    return static_cast<int32_t>(min + random_float * (max - min));
-}
-*/
+static int32_t max_int = std::numeric_limits<int32_t>::max();
 
 
+RandomInt::RandomInt(uint32_t seed)
+{
+    _seed = seed;
+    _distribution = std::uniform_int_distribution<int32_t>(0, max_int);
+    _generator = std::mt19937(seed);
+}
+
+int32_t RandomInt::unit()
+{
+    return _distribution(_generator);
+}
+
+int32_t RandomInt::signed_unit()
+{
+    return _distribution(_generator) * 2 - max_int;
+}
+
+int32_t RandomInt::range(int32_t min, int32_t max)
+{
+    return min + _distribution(_generator) * (max - min);
+}
--- Wolf/Wolf/numerical/Random.h
+++ Wolf/Wolf/numerical/Random.h
@@ -40,10 +40,27 @@ namespace Wolf
                     std::mt19937 _generator;
                     uint32_t _seed;
             };
+
+
+            class RandomInt
+            {
+            
+                public:
+                    RandomInt(uint32_t seed=0);
+                    int32_t unit();
+                    int32_t range(int32_t min, int32_t max);
+                    int32_t signed_unit();
+    
+                private:
+                    std::uniform_int_distribution<int32_t> _distribution;
+                    std::mt19937 _generator;
+                    uint32_t _seed;
+            };
     
             // Generic use static instances
             static thread_local RandomFloat RandomFloatGenerator;
             static thread_local RandomDouble RandomDoubleGenerator;
+            static thread_local RandomInt RandomIntGenerator;
     
             // Access to float methods
             static float unit_f() { return RandomFloatGenerator.unit(); }
@@ -54,6 +71,15 @@ namespace Wolf
             static double unit_d() { return RandomDoubleGenerator.unit(); }
             static double signed_unit_d() { return RandomDoubleGenerator.signed_unit(); }
             static double range_d(double min, double max) { return RandomDoubleGenerator.range(min, max); }
+            
+            // Access to double methods
+
+            /// @brief Returns an integer in the range [0, max(int32_t)]
+            static int32_t unit_i() { return RandomIntGenerator.unit(); }
+            static int32_t signed_unit_i() { return RandomIntGenerator.signed_unit(); }
+            static int32_t range_i(int32_t min, int32_t max) { return RandomIntGenerator.range(min, max); }
+            
+
         }
     }
 }
